63239c5872dddf71293a5b1636d56c0b2b283bd1
Hava-hizzle <mikehavrilak@gmail.com>
2020-03-23 22:25:19 -0400

    added components and functions to pages and components

--- reactportfolioapp/src/pages/about.js
+++ reactportfolioapp/src/pages/about.js
@@ -1,1 +1,5 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Row from "./components/Row";
+import Col from "./components/Col";
+import Container from "./components/Container"
