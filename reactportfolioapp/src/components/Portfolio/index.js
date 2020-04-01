ef0c8a9715abec67f4ac7562787c0034ddca156b
Hava-hizzle <mikehavrilak@gmail.com>
2020-03-31 21:36:57 -0400

    more styling fixes

--- reactportfolioapp/src/components/Portfolio/index.js
+++ reactportfolioapp/src/components/Portfolio/index.js
@@ -10,34 +10,34 @@ function Portfolio() {
                 <div className="work">
                  <img src="Assets/Images/30-Minute-Workouts.png" alt="Project#1" />
                  <a href="https://kshep425.github.io/30-Minute-Workouts" target="_blank">
-                    <h4>30 Minute Workout App</h4>
+                    <h5>30 Minute Workout App</h5>
                  </a>
 
                  </div>
                  <div className="work">
                 <img src="Assets/Images/Password-Generator.png" alt="Password Generator" />
                 <a href=" https://mhavrilak.github.io/Password-Generator/" target="_blank">
-                    <h4>Password Generator App</h4></a>
+                    <h5>Password Generator App</h5></a>
                 </div>
 
             <div className="work">
                 <img src="Assets/Images/Day-Planner.png" alt="Day Planner" />
                 <a href="https://mhavrilak.github.io/Day-Planner/" target="_blank">
-                    <h4>Day Planner</h4>
+                    <h5>Day Planner</h5>
                 </a>
 
             </div>
             <div className="work">
                 <img src="Assets/Images/Github-Portfolio.png" alt="Github Portfolio" />
                 <a href="https://github.com/MHavrilak" target="_blank">
-                    <h4>Github Profile</h4>
+                    <h5>Github Profile</h5>
                 </a>
             
             </div>
             <div className="work">
                 <img src="Assets/Images/yappE.png" alt="Project Two" />
                 <a href="https://yappe.herokuapp.com/" target="_blank">
-                    <h4>yappE</h4>
+                    <h5>yappE</h5>
                 </a>
             </div>
         </section>
